fff5b94e25cb9b8b514fd8eb1038c7ae989f61f8
Andrew Marmion <andrew.marmion@gmail.com>
2021-05-08 11:12:57 +0100

    Initial Commit

---
+++ MyToDos/MyToDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDosApp.swift
+//  MyToDos
+//
+//  Created by Andrew Marmion on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
