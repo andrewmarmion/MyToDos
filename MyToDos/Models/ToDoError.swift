b45c8110447db89a9f722c7d246bfa0a7e3075ea
Andrew Marmion <andrew.marmion@gmail.com>
2021-05-10 17:36:50 +0100

    handled errors:)

---
+++ MyToDos/Models/ToDoError.swift
@@ -0,0 +1,34 @@
+//
+//  ToDoError.swift
+//  MyToDos
+//
+//  Created by Andrew Marmion on 10/05/2021.
+//
+
+import Foundation
+
+enum ToDoError: Error, LocalizedError {
+    case saveError
+    case readError
+    case decodingError
+    case encodingError
+
+    var errorDescription: String? {
+        switch self {
+
+            case .saveError:
+                return NSLocalizedString("Could not save ToDos, please reinstall the app.", comment: "")
+            case .readError:
+                return NSLocalizedString("Could not load ToDos, please reinstall the app.", comment: "")
+            case .decodingError:
+                return NSLocalizedString("There was a problem loading your ToDos, please create a new ToDo to start over.", comment: "")
+            case .encodingError:
+                return NSLocalizedString("Could not save ToDos, please reinstall the app.", comment: "")
+        }
+    }
+}
+
+struct ErrorType: Identifiable {
+    let id = UUID()
+    let error: ToDoError
+}
