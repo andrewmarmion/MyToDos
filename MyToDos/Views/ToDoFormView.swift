788e6c4dad35c385d5355a1214442f78464a5b65
Andrew Marmion <andrew.marmion@gmail.com>
2021-05-11 18:06:51 +0100

    added passthrough subject

--- MyToDos/Views/ToDoFormView.swift
+++ MyToDos/Views/ToDoFormView.swift
@@ -30,14 +30,14 @@ struct ToDoFormView: View {
 extension ToDoFormView {
     func updateToDo() {
         let toDo = ToDo(id: formVM.id!, name: formVM.name, completed: formVM.completed)
-        dataStore.updateToDo(toDo)
+        dataStore.updateToDo.send(toDo)
         presentationMode.wrappedValue.dismiss()
     }
 
     func addToDo() {
         let toDo = ToDo(name: formVM.name)
         presentationMode.wrappedValue.dismiss()
-        dataStore.addToDo(toDo)
+        dataStore.addToDo.send(toDo)
     }
 
     var cancelButton: some View {
